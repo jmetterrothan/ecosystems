ff961a1c39c7517c4417444e4954671ad39f31d1
florian <zobeleflorian@gmail.com>
2018-11-21 15:05:02 +0100

    build: add three types for typescript

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,15 +1,2 @@
 // import * as THREE from 'three';
 // import 'three/examples/js/controls/OrbitControls';
-
-class Test {
-
-  a: number = 1;
-
-  init() {
-    console.log(a);
-  }
-
-}
-
-const a = new Test();
-a.init();
