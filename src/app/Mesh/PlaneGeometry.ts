0f24948aae450996cbf2aa99c55bd74f27dafd4c
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2018-12-03 23:02:51 +0100

    feat: added plane geometry

---
+++ src/app/Mesh/PlaneGeometry.ts
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+
+class PlaneGeometry
+{
+  static create(nrows: number, ncols: number, cellsize: number) {
+    const geometry: THREE.Geometry = new THREE.Geometry();
+
+    const nbVerticesX = ncols + 1;
+    const nbVerticesZ = nrows + 1;
+
+    // creates all our vertices
+    for (let col = 0; col < nbVerticesX; col++) {
+      for (let row = 0; row < nbVerticesZ; row++) {
+        geometry.vertices.push(new THREE.Vector3(col * cellsize, 0, row * cellsize));
+      }
+    }
+
+    for (let col = 0; col < ncols; col++) {
+      for (let row = 0; row < nrows; row++) {
+        const a = col + nbVerticesX * row;
+        const b = (col + 1) + nbVerticesX * row;
+        const c = col + nbVerticesX * (row + 1);
+        const d = (col + 1) + nbVerticesX * (row + 1);
+
+        geometry.faces.push(new THREE.Face3(a, b, d));
+        geometry.faces.push(new THREE.Face3(d, c, a));
+      }
+    }
+
+    // need to tell the engine we updated the vertices
+    geometry.verticesNeedUpdate = true;
+    geometry.colorsNeedUpdate = true;
+
+    // need to update normals for smooth shading
+    geometry.computeFaceNormals();
+    geometry.computeVertexNormals();
+    geometry.normalsNeedUpdate = true;
+    console.log(geometry);
+    return geometry;
+  }
+}
+
+export default PlaneGeometry;
