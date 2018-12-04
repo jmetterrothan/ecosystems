21b59ac559fafd8bcb75fea7829d7fb0ade52089
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2018-12-04 12:37:43 +0100

    feat: improved terrain material

--- src/app/Shared/materials/terrain.material.ts
+++ src/app/Shared/materials/terrain.material.ts
@@ -2,11 +2,11 @@ import * as THREE from 'three';
 
 export const TERRAIN_MATERIAL: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({
   wireframe: false,
-  emissive: 0x505050,
-  emissiveIntensity: 0.01,
-  specular: 0x353535,
-  shininess: 6,
-  reflectivity: 0.1,
+  emissive: 0xffffff,
+  emissiveIntensity: 0.05,
+  specular: 0x101010,
+  shininess: 4,
+  reflectivity: 0,
   flatShading: true,
   vertexColors: THREE.FaceColors
 });
