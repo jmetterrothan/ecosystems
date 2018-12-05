489144f2db2f48ffa17c439a54bc04ce268774a4
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2018-12-05 10:31:50 +0100

    feat: cloud formations over wet areas

--- src/app/Mesh/TerrainMesh.ts
+++ src/app/Mesh/TerrainMesh.ts
@@ -42,7 +42,8 @@ class TerrainMesh extends Mesh {
   }
 
   needGenerateCloud(): boolean {
-    return this.moistureAverage > 0.33 && MathUtils.rng() > 0.925;
+    const t = (this.moistureAverage > 0.66) ? (1 - this.moistureAverage) + 0.5 : 0.975;
+    return this.moistureAverage > 0.34 && MathUtils.rng() > t;
   }
 }
 
