530ff6db7a0999907530fcad5546446f5f950959
florian <zobeleflorian@gmail.com>
2018-12-05 13:08:04 +0100

    refactor: modify mesh generation

--- src/app/Mesh/Mesh.ts
+++ src/app/Mesh/Mesh.ts
@@ -36,79 +36,81 @@ abstract class Mesh {
   abstract getY(x?: number, z?: number): number;
   abstract getMaterial(): THREE.Material;
 
-  buildGeometry(): THREE.Geometry {
-    const geometry: THREE.Geometry = new THREE.Geometry();
+  abstract buildGeometry(): THREE.Geometry;
 
-    const nbVerticesX = this.parameters.nCols + 1;
-    const nbVerticesZ = this.parameters.nRows + 1;
+  // buildGeometry(): THREE.Geometry {
+  //   const geometry: THREE.Geometry = new THREE.Geometry();
 
-    // creates all our vertices
-    for (let c = 0; c < nbVerticesX; c++) {
-      const x = this.col * this.parameters.width + c * this.parameters.cellSizeX;
-      for (let r = 0; r < nbVerticesZ; r++) {
-        const z = this.row * this.parameters.depth + r * this.parameters.cellSizeZ;
-        const y = this.getY(x, z);
+  //   const nbVerticesX = this.parameters.nCols + 1;
+  //   const nbVerticesZ = this.parameters.nRows + 1;
 
-        if (this.low === null || this.low > y) this.low = y;
-        if (this.high === null || this.high < y) this.high = y;
+  //   // creates all our vertices
+  //   for (let c = 0; c < nbVerticesX; c++) {
+  //     const x = this.col * this.parameters.width + c * this.parameters.cellSizeX;
+  //     for (let r = 0; r < nbVerticesZ; r++) {
+  //       const z = this.row * this.parameters.depth + r * this.parameters.cellSizeZ;
+  //       const y = this.getY(x, z);
 
-        geometry.vertices.push(new THREE.Vector3(x, y, z));
-      }
-    }
+  //       if (this.low === null || this.low > y) this.low = y;
+  //       if (this.high === null || this.high < y) this.high = y;
 
-    // creates the associated faces with their indexes
-    let sumMoisture = 0;
+  //       geometry.vertices.push(new THREE.Vector3(x, y, z));
+  //     }
+  //   }
 
-    for (let col = 0; col < this.parameters.nCols; col++) {
-      for (let row = 0; row < this.parameters.nRows; row++) {
-        const a = col + nbVerticesX * row;
-        const b = (col + 1) + nbVerticesX * row;
-        const c = col + nbVerticesX * (row + 1);
-        const d = (col + 1) + nbVerticesX * (row + 1);
+  //   // creates the associated faces with their indexes
+  //   let sumMoisture = 0;
 
-        const f1 = new THREE.Face3(a, b, d);
-        const f2 = new THREE.Face3(d, c, a);
+  //   for (let col = 0; col < this.parameters.nCols; col++) {
+  //     for (let row = 0; row < this.parameters.nRows; row++) {
+  //       const a = col + nbVerticesX * row;
+  //       const b = (col + 1) + nbVerticesX * row;
+  //       const c = col + nbVerticesX * (row + 1);
+  //       const d = (col + 1) + nbVerticesX * (row + 1);
 
-        const x1 = (geometry.vertices[a].x + geometry.vertices[b].x + geometry.vertices[d].x) / 3;
-        const x2 = (geometry.vertices[d].x + geometry.vertices[c].x + geometry.vertices[a].x) / 3;
+  //       const f1 = new THREE.Face3(a, b, d);
+  //       const f2 = new THREE.Face3(d, c, a);
 
-        const y1 = (geometry.vertices[a].y + geometry.vertices[b].y + geometry.vertices[d].y) / 3;
-        const y2 = (geometry.vertices[d].y + geometry.vertices[c].y + geometry.vertices[a].y) / 3;
+  //       const x1 = (geometry.vertices[a].x + geometry.vertices[b].x + geometry.vertices[d].x) / 3;
+  //       const x2 = (geometry.vertices[d].x + geometry.vertices[c].x + geometry.vertices[a].x) / 3;
 
-        const z1 = (geometry.vertices[a].z + geometry.vertices[b].z + geometry.vertices[d].z) / 3;
-        const z2 = (geometry.vertices[d].z + geometry.vertices[c].z + geometry.vertices[a].z) / 3;
+  //       const y1 = (geometry.vertices[a].y + geometry.vertices[b].y + geometry.vertices[d].y) / 3;
+  //       const y2 = (geometry.vertices[d].y + geometry.vertices[c].y + geometry.vertices[a].y) / 3;
 
-        const m1 = this.generator.computeMoisture(x1, z1);
-        const m2 = this.generator.computeMoisture(x2, z2);
+  //       const z1 = (geometry.vertices[a].z + geometry.vertices[b].z + geometry.vertices[d].z) / 3;
+  //       const z2 = (geometry.vertices[d].z + geometry.vertices[c].z + geometry.vertices[a].z) / 3;
 
-        if (this.type === MESH_TYPES.TERRAIN_MESH) {
-          f1.color = this.generator.getBiome(BiomeGenerator.getElevationFromHeight(y1), m1).color;
-          f2.color = this.generator.getBiome(BiomeGenerator.getElevationFromHeight(y2), m2).color;
-        } else if (this.type === MESH_TYPES.WATER_MESH) {
-          f1.color = this.generator.getWaterColor(m1);
-          f2.color = this.generator.getWaterColor(m2);
-        }
+  //       const m1 = this.generator.computeMoisture(x1, z1);
+  //       const m2 = this.generator.computeMoisture(x2, z2);
 
-        geometry.faces.push(f1);
-        geometry.faces.push(f2);
+  //       if (this.type === MESH_TYPES.TERRAIN_MESH) {
+  //         f1.color = this.generator.getBiome(BiomeGenerator.getElevationFromHeight(y1), m1).color;
+  //         f2.color = this.generator.getBiome(BiomeGenerator.getElevationFromHeight(y2), m2).color;
+  //       } else if (this.type === MESH_TYPES.WATER_MESH) {
+  //         f1.color = this.generator.getWaterColor(m1);
+  //         f2.color = this.generator.getWaterColor(m2);
+  //       }
 
-        sumMoisture += m1 + m2;
-      }
-    }
+  //       geometry.faces.push(f1);
+  //       geometry.faces.push(f2);
 
-    this.moistureAverage = sumMoisture / (this.parameters.nCols * this.parameters.nRows * 2);
+  //       sumMoisture += m1 + m2;
+  //     }
+  //   }
 
-    // need to tell the engine we updated the vertices
-    geometry.verticesNeedUpdate = true;
-    geometry.colorsNeedUpdate = true;
+  //   this.moistureAverage = sumMoisture / (this.parameters.nCols * this.parameters.nRows * 2);
 
-    // need to update normals for smooth shading
-    geometry.computeFaceNormals();
-    geometry.computeVertexNormals();
-    geometry.normalsNeedUpdate = true;
+  //   // need to tell the engine we updated the vertices
+  //   geometry.verticesNeedUpdate = true;
+  //   geometry.colorsNeedUpdate = true;
 
-    return geometry;
-  }
+  //   // need to update normals for smooth shading
+  //   geometry.computeFaceNormals();
+  //   geometry.computeVertexNormals();
+  //   geometry.normalsNeedUpdate = true;
+
+  //   return geometry;
+  // }
 
   generate(): THREE.Mesh {
     const mesh = new THREE.Mesh(this.buildGeometry(), this.getMaterial());
--- src/app/Mesh/TerrainMesh.ts
+++ src/app/Mesh/TerrainMesh.ts
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 import Mesh from '@mesh/Mesh';
 import World from '@world/World';
 import Chunk from '@world/Chunk';
@@ -45,6 +47,75 @@ class TerrainMesh extends Mesh {
     const t = (this.moistureAverage > 0.66) ? (1 - this.moistureAverage) + 0.55 : 0.975;
 
     return this.moistureAverage > 0.34 && MathUtils.rng() > t;
+  }
+
+  buildGeometry(): THREE.Geometry {
+    const geometry: THREE.Geometry = new THREE.Geometry();
+
+    const nbVerticesX = this.parameters.nCols + 1;
+    const nbVerticesZ = this.parameters.nRows + 1;
+
+    // creates all our vertices
+    for (let c = 0; c < nbVerticesX; c++) {
+      const x = this.col * this.parameters.width + c * this.parameters.cellSizeX;
+      for (let r = 0; r < nbVerticesZ; r++) {
+        const z = this.row * this.parameters.depth + r * this.parameters.cellSizeZ;
+        const y = this.getY(x, z);
+
+        if (this.low === null || this.low > y) this.low = y;
+        if (this.high === null || this.high < y) this.high = y;
+
+        geometry.vertices.push(new THREE.Vector3(x, y, z));
+      }
+    }
+
+    // creates the associated faces with their indexes
+    let sumMoisture = 0;
+
+    for (let col = 0; col < this.parameters.nCols; col++) {
+      for (let row = 0; row < this.parameters.nRows; row++) {
+        const a = col + nbVerticesX * row;
+        const b = (col + 1) + nbVerticesX * row;
+        const c = col + nbVerticesX * (row + 1);
+        const d = (col + 1) + nbVerticesX * (row + 1);
+
+        const f1 = new THREE.Face3(a, b, d);
+        const f2 = new THREE.Face3(d, c, a);
+
+        const x1 = (geometry.vertices[a].x + geometry.vertices[b].x + geometry.vertices[d].x) / 3;
+        const x2 = (geometry.vertices[d].x + geometry.vertices[c].x + geometry.vertices[a].x) / 3;
+
+        const y1 = (geometry.vertices[a].y + geometry.vertices[b].y + geometry.vertices[d].y) / 3;
+        const y2 = (geometry.vertices[d].y + geometry.vertices[c].y + geometry.vertices[a].y) / 3;
+
+        const z1 = (geometry.vertices[a].z + geometry.vertices[b].z + geometry.vertices[d].z) / 3;
+        const z2 = (geometry.vertices[d].z + geometry.vertices[c].z + geometry.vertices[a].z) / 3;
+
+        const m1 = this.generator.computeMoisture(x1, z1);
+        const m2 = this.generator.computeMoisture(x2, z2);
+
+        f1.color = this.generator.getBiome(BiomeGenerator.getElevationFromHeight(y1), m1).color;
+        f2.color = this.generator.getBiome(BiomeGenerator.getElevationFromHeight(y2), m2).color;
+
+        geometry.faces.push(f1);
+        geometry.faces.push(f2);
+
+        sumMoisture += m1 + m2;
+      }
+    }
+
+    this.moistureAverage = sumMoisture / (this.parameters.nCols * this.parameters.nRows * 2);
+
+    // need to tell the engine we updated the vertices
+    geometry.verticesNeedUpdate = true;
+    geometry.colorsNeedUpdate = true;
+
+    // need to update normals for smooth shading
+    geometry.computeFaceNormals();
+    geometry.computeVertexNormals();
+    geometry.normalsNeedUpdate = true;
+
+    return geometry;
   }
 }
 
