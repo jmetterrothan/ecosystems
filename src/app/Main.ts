8e44adae816980d47549eee75a0006dd956fb91c
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2018-12-07 00:42:48 +0100

    fix: camera fov

--- src/app/Main.ts
+++ src/app/Main.ts
@@ -37,7 +37,7 @@ class Main {
     document.body.appendChild(this.stats.dom);
 
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, World.VIEW_DISTANCE);
+    this.camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 10, World.VIEW_DISTANCE);
   }
 
   async init() {
