01546caf7bf4e9192b143cc19cbe59b0f43f25b9
Flroian <zobeleflorian@gmail.com>
2018-12-30 11:48:26 +0100

    feat: random biome

--- src/app/World/BiomeGenerator.ts
+++ src/app/World/BiomeGenerator.ts
@@ -1,4 +1,3 @@
-import OceanBiome from '@world/Biomes/OceanBiome';
 import * as THREE from 'three';
 import simplexNoise from 'simplex-noise';
 
@@ -17,7 +16,7 @@ import { IPickObject } from '@shared/models/objectParameters.model';
 
 class BiomeGenerator {
   // @ts-ignore
-  public static readonly BIOME: Biome | null = OceanBiome; // lock a specific biome here, if null a biome is selected randomly
+  public static readonly BIOME: Biome | null = null; // lock a specific biome here, if null a biome is selected randomly
 
   private simplex: simplexNoise;
   private simplex2: simplexNoise;
