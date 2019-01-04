753e0bc0dc785ffc018fd791665dd33e15cdec11
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2019-01-04 16:32:43 +0100

    feat: disabled vulture

--- src/app/World/Biomes/DesertBiome.ts
+++ src/app/World/Biomes/DesertBiome.ts
@@ -12,7 +12,7 @@ import { IPick } from '@shared/models/pick.model';
 import MathUtils from '@shared/utils/Math.utils';
 
 class DesertBiome extends Biome {
-  private vulture: THREE.Object3D;
+  // private vulture: THREE.Object3D;
 
   constructor(generator: BiomeGenerator) {
     super('DESERT', generator);
@@ -43,15 +43,17 @@ class DesertBiome extends Biome {
 
     chunk.placeObject(corpseObject, { save: true });
 
+    /*
     // vulture
     this.vulture = chunk.getObject({ ...corpseItem });
     this.vulture.position.setY(Chunk.CLOUD_LEVEL);
     this.vulture.children.forEach((obj: THREE.Mesh) => obj.translateX(-20));
     chunk.placeObject(this.vulture, { save: true });
+    */
   }
 
   update(delta: number) {
-    this.vulture.rotateOnAxis(new THREE.Vector3(0, 1, 0), THREE.Math.degToRad(0.4));
+    // this.vulture.rotateOnAxis(new THREE.Vector3(0, 1, 0), THREE.Math.degToRad(0.4));
   }
 
   /**
