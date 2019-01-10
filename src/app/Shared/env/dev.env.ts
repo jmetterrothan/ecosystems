a4c3b9e906883b322a6c1a5a00f6c986cd3b49cf
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2019-01-10 23:36:28 +0100

    fix: config heroku

--- src/app/Shared/env/dev.env.ts
+++ src/app/Shared/env/dev.env.ts
@@ -1,6 +1,6 @@
 import { IEnv } from '@shared/models/env.model';
 
 export const DEV_ENV: IEnv = {
-  socketBaseUrl: 'ws://localhost',
-  socketPort: 4200
+  socketBaseUrl: 'ws://ecosystem-server.herokuapp.com/',
+  socketPort: 32864
 };
