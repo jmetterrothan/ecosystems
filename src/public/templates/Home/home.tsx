7cecca30f3512fd95e98d484199a8b287e6bbdc5
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2019-01-14 17:47:51 +0100

    feat: added grid

--- src/public/templates/Home/home.tsx
+++ src/public/templates/Home/home.tsx
@@ -5,7 +5,13 @@ import Button from '@components/Button/button';
 const Home = () => (
   <>
     <h1>Home page</h1>
-    <Button onClick={() => console.log('here')}>le bouton</Button>
+    <div className="flexgrid">
+      <div className="col col_12">
+        1
+        <Button onClick={() => console.log('here')}>le bouton</Button>
+      </div>
+      <div className="col col_12">2</div>
+    </div>
   </>
 );
 
