34c91caee1bf22e4d4f8880e97540228af45d510
florian <zobeleflorian@gmail.com>
2019-01-15 17:28:14 +0100

    refactor!: remove useless Input component

--- src/public/templates/Home/home.tsx
+++ src/public/templates/Home/home.tsx
@@ -3,28 +3,41 @@ import React from 'react';
 import Row from '@components/Row/row';
 import Col from '@components/Col/col';
 import Button from '@components/Button/button';
-import Input from '@components/Input/input';
 
 import { IServices } from '@ui/models/services.model';
 
 import { UI_STATES } from '@ui/enums/UIStates.enum';
 
-const Home = ({ uiManager }: IServices) => (
-  <>
-    <Row justify='center'>
-      <Col className='col_24' textAlign='center' style={{ margin: '60px 0' }}>Ecosystem</Col>
-    </Row>
-    <Row justify='center'>
-      <Col className='col_6'>
-        <Input className='full' placeholder='seed' required />
-      </Col>
-    </Row>
-    <Row justify='center'>
-      <Col className='col_6' textAlign='center' style={{ margin: 20 }}>
-        <Button className='full' onClick={() => uiManager.switchState(UI_STATES.PLAY)}>Jouer</Button>
-      </Col>
-    </Row>
-  </>
-);
+const Home = ({ uiManager }: IServices) => {
+
+  let form: HTMLFormElement;
+
+  const handleSubmit = ev => {
+    ev.preventDefault();
+    console.log(form.checkValidity());
+  };
+
+  return (
+    <>
+      <Row justify='center'>
+        <Col className='col_24' textAlign='center' style={{ margin: '60px 0' }}>Ecosystem</Col>
+      </Row>
+      <Row justify='center'>
+        <Col className='col_6'>
+          <form onSubmit={handleSubmit} ref={el => form = el}>
+            <input type='text' className='full' placeholder='seed' pattern='^[a-zA-Z]+$' minLength={6} required />
+            <input type='submit' value='jouer' className='full' />
+          </form>
+        </Col>
+      </Row>
+      {/* <Row justify='center'>
+        <Col className='col_6' textAlign='center' style={{ margin: 20 }}>
+          <Button className='full' onClick={() => uiManager.switchState(UI_STATES.PLAY)}>Jouer</Button>
+        </Col>
+      </Row> */}
+    </>
+  );
+
+};
 
 export default Home;
