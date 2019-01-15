f3f9548c8a8986ac6ece5fb782d16c5788d4c0a7
florian <zobeleflorian@gmail.com>
2019-01-16 00:12:50 +0100

    feat: enable or disable interface mode

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,16 @@ import ReactDOM from 'react-dom';
 import Main from '@app/Main';
 import UIManager from '@ui/UIManager';
 
-ReactDOM.render(
-  <UIManager />,
-  document.getElementById('root')
-);
+if (UIManager.ENABLED) {
+  ReactDOM.render(
+    <UIManager />,
+    document.getElementById('root')
+  );
+} else {
+  (async () => {
+    const app = new Main();
+    await app.init();
+    await app.load();
+    app.run();
+  })();
+}
