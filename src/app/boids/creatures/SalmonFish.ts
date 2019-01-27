09a0ea01ad49bf4469085544d19e5e5bb388b9d2
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2019-01-27 01:42:39 +0100

    fix: salmon behaviour

--- src/app/boids/creatures/SalmonFish.ts
+++ src/app/boids/creatures/SalmonFish.ts
@@ -5,14 +5,14 @@ class SalmonFish extends Creature {
   constructor() {
     super(['fish2'], {
       speed: 6750,
-      neighbourRadius: 10000,
+      neighbourRadius: 6000,
       alignmentWeighting: 0.0065,
       cohesionWeighting: 0.01,
-      separationWeighting: 0.35,
-      viewAngle: 12,
+      separationWeighting: 0.1,
+      viewAngle: 8,
       underwater: true,
       minRepulseDistance: 30000,
-      scale: MathUtils.randomFloat(0.75, 1.25)
+      scale: MathUtils.randomFloat(0.25, 0.75)
     });
   }
 }
