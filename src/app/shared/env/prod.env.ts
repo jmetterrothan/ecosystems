05a8d2f0d9dd0f91b893784de4ea8daf309fb3e2
Flroian <zobeleflorian@gmail.com>
2019-02-20 10:02:17 +0100

    fix: prod env

--- src/app/shared/env/prod.env.ts
+++ src/app/shared/env/prod.env.ts
@@ -1,6 +1,6 @@
 import { IEnv } from '@shared/models/env.model';
 
 export const PROD_ENV: IEnv = {
-  socketBaseUrl: 'wss://192.168.43.230/',
-  socketPort: 32864
+  socketBaseUrl: 'ws://192.168.43.230/',
+  socketPort: 4200
 };
