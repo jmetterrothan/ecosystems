d8f2de62ff409589e6348110383c10ff0343224c
Flroian <zobeleflorian@gmail.com>
2019-02-20 09:44:58 +0100

    feat: prod env url

--- src/app/shared/env/prod.env.ts
+++ src/app/shared/env/prod.env.ts
@@ -1,6 +1,6 @@
 import { IEnv } from '@shared/models/env.model';
 
 export const PROD_ENV: IEnv = {
-  socketBaseUrl: 'ws://ecosystem-server.herokuapp.com/',
+  socketBaseUrl: 'ws://192.168.43.230/',
   socketPort: 32864
 };
