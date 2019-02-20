714f8bd357656a9307caf7c3b79988067175b6cd
Flroian <zobeleflorian@gmail.com>
2019-02-20 09:48:29 +0100

    fix: wss socket

--- src/app/shared/env/prod.env.ts
+++ src/app/shared/env/prod.env.ts
@@ -1,5 +1,10 @@
 import { IEnv } from '@shared/models/env.model';
 
 export const PROD_ENV: IEnv = {
+<<<<<<< develop
   socketBaseUrl: 'ws://ecosystem-server.herokuapp.com'
+=======
+  socketBaseUrl: 'wss://192.168.43.230/',
+  socketPort: 32864
+>>>>>>> fix: wss socket
 };
